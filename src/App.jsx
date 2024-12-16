56be2da6765cdd30615f3700b8af5b6fdf140e84
Cristian Marsico <cristianmarsico84@gmail.com>
2024-12-16 02:53:50 -0300

    se agrega la carpeta dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import Home from "./pages/Home";
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import AuthPage from "./pages/AuthPage";
 import Error from "./pages/Error";
-import { AuthProvider } from "./context/AuthProvider";
-import PrivateRoutes from "./routes/PrivateRoutes";
 import FormAddDdjj from "./pages/FormAddDdjj";
 import DetailsTaxpayer from "./pages/DetailsTaxpayer";
+import { AuthProvider } from "./context/AuthProvider";
+import PrivateRoutes from "./routes/PrivateRoutes";
 
 function App() {
   return (
